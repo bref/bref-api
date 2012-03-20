5ff7ba3de32b8f13479881a45fceb8119ad2f848
Vincent Giersch <vincent@giersch.fr>
2012-03-20 18:06:19 +0100

    Add missing detail/BrefDLL.h file

---
+++ include/bref/detail/BrefDLL.h
@@ -0,0 +1,24 @@
+/**
+ * \file   BrefDLL.h
+ * \author Vincent Giersch
+ * \date   Tue Mar 20 16:21:00 2012
+ */
+
+#ifndef BREF_API_DETAIL_BREFDLL_H_
+#define BREF_API_DETAIL_BREFDLL_H_
+
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef __GNUC__
+    #define BREF_DLL __attribute__ ((dllexport))
+  #else
+    #define BREF_DLL __declspec(dllexport)
+  #endif
+#else
+  #if __GNUC__ >= 4
+    #define BREF_DLL __attribute__ ((visibility ("default")))
+  #else
+    #define BREF_DLL
+  #endif
+#endif
+
+#endif /* !BREF_API_DETAIL_BREFDLL_H_ */
