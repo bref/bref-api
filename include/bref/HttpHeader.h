523310a3ff5bb0acfabe7beb5e80cc4df126f75f
Guillaume Papin <guillaume.papin@epitech.eu>
2012-04-12 02:34:18 +0200

    Use a case insensitive comparator for message-header field name.

--- include/bref/HttpHeader.h
+++ include/bref/HttpHeader.h
@@ -13,14 +13,21 @@
 #include <map>
 #include <string>
 #include "BrefValue.h"
+#include "detail/util/ICaseStringCmp.hpp"
 
 namespace bref {
 
 /**
  * \brief A map seems to be sufficient to represent HTTP Header fields.
+ *
+ * \note RFC2616, section 4.2 says: "Field names are
+ *       case-insensitive.". The third parameter util::ICaseStringCmp
+ *       allows the map to consider the keys "KEY", "key", "Key", etc,
+ *       to be equivalent.
+ *
  * \sa BrefValue
  */
-typedef std::map<std::string, BrefValue> HttpHeader;
+typedef std::map<std::string, BrefValue, util::ICaseStringCmp> HttpHeader;
 
 } // ! bref
 
---
+++ include/bref/detail/util/ICaseStringCmp.hpp
@@ -0,0 +1,46 @@
+/**
+ * \file   ICaseStringCmp.hpp
+ * \author Guillaume Papin <guillaume.papin@epitech.eu>
+ * \date   Thu Apr 12 02:12:52 2012
+ *
+ * \brief  ICaseStringCmp class definition.
+ *
+ */
+
+#ifndef BREF_DETAIL_UTIL_ICASESTRINGCMP_HPP_
+#define BREF_DETAIL_UTIL_ICASESTRINGCMP_HPP_
+
+#pragma once
+
+#include <string>
+#include <functional>
+#include <cctype>
+
+namespace bref {
+namespace util {
+
+/**
+ * \brief Case insensitive char comparision function.
+ */
+inline bool icaseCharCmp(char a, char b)
+{
+  return std::tolower(a) < std::tolower(b);
+}
+
+/**
+ * \brief Case insensitive string comparison.
+ */
+struct ICaseStringCmp : public std::binary_function<std::string, std::string, bool>
+{
+  bool operator()(const std::string & a, const std::string & b)
+  {
+    return std::lexicographical_compare(a.begin(), a.end(),
+                                        b.begin(), b.end(),
+                                        &icaseCharCmp);
+  }
+};
+
+} // ! util
+} // ! bref
+
+#endif /* !BREF_DETAIL_UTIL_ICASESTRINGCMP_HPP_ */
